52c6ef21919c83a6d2dcae868470890f8eec237f
andrey678 <drew6496@yandex.ru>
2021-09-21 20:42:39 +0300

    1.Add section 'main' 2.Add CSS for section 'main' 3.Add 'menu burger' 4.Add js script for changing right button text in section 'main' 5.Add section 'stirrer' 6.Add media query grid 7.Add media query behavior for sections 'main' and 'stirrer' with a max-width of 450px

--- layout_8_nevelvend/js/script.js
+++ layout_8_nevelvend/js/script.js
@@ -1,3 +1,4 @@
+// CHECK FOR WEBP SUPPORT
 function testWebP(callback) {
 
 var webP = new Image();
@@ -15,3 +16,122 @@ document.querySelector('body').classList.add('webp');
 document.querySelector('body').classList.add('no-webp');
 }
 });
+
+// BURGER MENU
+$(document).ready(function(){
+  $('.header__burger').click(function(event){
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+  $('.menu__link').click(function(event){
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+
+// CHANGE RIGHT BUTTON TEXT
+
+let rightButton = document.querySelector('.main__link:last-child');
+const mediaQuery = window.matchMedia('(max-width: 450px)');
+console.log(mediaQuery);
+
+if (mediaQuery.matches){
+  rightButton.innerHTML = `Подробнее`;
+} else {
+  rightButton.innerHTML = `Узнать больше`;
+};
+console.log(rightButton);
+
+// STIRRER
+
+var theWindow = $(window); 
+
+var winHeight = theWindow.height(); 
+
+var animDuration = winHeight / 2; 
+
+var animData = { 
+
+container: document.getElementById('lottie-three'), 
+
+renderer: 'svg', 
+
+loop: false, 
+
+autoplay: false, 
+
+path: 'json/MixerSequence.json' 
+
+}; 
+
+var anim = bodymovin.loadAnimation(animData); 
+
+$( window ).scroll(function() { 
+
+animatebodymovin(animDuration, anim); 
+
+}); 
+
+function animatebodymovin(duration, animObject) { 
+
+var scrollPosition = theWindow.scrollTop(); 
+
+var maxFrames = animObject.totalFrames; 
+
+var frame = (maxFrames / 100) * (scrollPosition / (duration / 100)); 
+
+animObject.goToAndStop(frame, true); 
+
+} 
+
+anim.addEventListener("enterFrame", function (animation) { 
+
+if (animation.currentTime > (anim.totalFrames - 1)) { 
+
+animObject.pause(); 
+
+//anim.pause(); 
+
+} 
+
+}); 
+
+
+// ==============================================================================
+// var animData = {
+//   container: document.getElementById('lottie-three'),
+//   path: 'json/MixerSequence.json',
+//   renderer: 'svg',
+//   loop: false,
+//   autoplay: false,
+//   name: "animScroll",
+// }, animScroll, tl;
+
+
+// var animScroll = bodymovin.loadAnimation(animData)
+
+
+// animScroll.addEventListener('DOMLoaded', function () {
+//   tl = new TimelineMax({repeat: 0})
+//   tl.to({frame: 0}, 1, {
+//     frame: animScroll.totalFrames-1,
+//     onUpdate: function() {
+//       animScroll.goToAndStop(Math.round(this.target.frame), true)
+//     },
+//     Ease:Linear.easeNone
+//   })
+  
+// var controller = new ScrollMagic.Controller();
+
+// var scene = new ScrollMagic.Scene({
+//   triggerElement: ".env-three",
+//   offset: 500,
+//   duration: 5000 }).setTween(tl).setPin("#lottie-three").addTo(controller);
+
+// })
+
+// $(window).load(function(){
+//    // $("#lottie-three svg").removeAttr('viewBox');
+//    // $("#lottie-three svg").attr('viewBox','0 0 400 1240');
+//    // $("#lottie-three svg").attr('viewBox','0 0 300 1140');
+// })
